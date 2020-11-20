751c1006d11659ab9a44dcd6f94bcbdc456c55ba
Fabiano Maia <fmaiati@gmail.com>
2020-11-20 20:29:22 -0300

    Estado e cidade

---
+++ src/main/java/com/fmdev/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.fmdev.cursomc.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fmdev.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
